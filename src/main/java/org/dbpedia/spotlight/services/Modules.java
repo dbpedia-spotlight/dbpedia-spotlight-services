c4f2578efe6876c30e507ac00e7ed62dff01ab0b
Sandro <sandroacoelho@gmail.com>
2017-01-13 00:30:40 -0200

    Update Modules.java

--- src/main/java/org/dbpedia/spotlight/services/Modules.java
+++ src/main/java/org/dbpedia/spotlight/services/Modules.java
@@ -3,7 +3,4 @@ package org.dbpedia.spotlight.services;
 public interface Modules {
 
     String SPOTLIGHT_LUCENE = "SPOTLIGHT-LUCENE";
-
-    String SPOTLIGHT_LUCENE_PT = "SPOTLIGHT-LUCENE-PT";
-
 }
